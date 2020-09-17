6fb50a704de8b98c774613faccb8fb3d54c4f083
Chen Wenwen (CR/AME5) <Wenwen.Chen@de.bosch.com>
2020-09-17 16:24:18 +0200

    rewrite to class
    Signed-off-by: Chen Wenwen (CR/AME5) <Wenwen.Chen@de.bosch.com>

--- invehicle-apps/kuksa-traccar-client/providers/gpsd.py
+++ invehicle-apps/kuksa-traccar-client/providers/gpsd.py
@@ -16,75 +16,70 @@
 from gps3 import gps3
 import sys
 import threading
-lock=threading.Lock()
 
-RUNNING=True
+class Provider():
+    def __init__(self, config):
+        print("Init gpsd provider...")
+        if "Provider.gpsd" not in config:
+            print("Provider.gpsd section missing from configuration, exiting")
+            sys.exit(-1)
+        
+        provider_config=config['Provider.gpsd']
+        gpsd_host=provider_config.get('host','127.0.0.1')
+        gpsd_port=provider_config.get('port','2947')
 
+        print("Trying to connect gpsd at "+str(gpsd_host)+" port "+str(gpsd_port))
+        self.connect(gpsd_host,gpsd_port)
 
-position = { "valid":False, "lat":"0", "lon":"0", "alt":"0", "hdop":"0", "speed":"0" }
+        self.position = { "valid":False, "lat":"0", "lon":"0", "alt":"0", "hdop":"0", "speed":"0" }
+        self.running = True
+        self.lock = threading.Lock()
 
-def loop(gpsd_socket):
-    global position,lock, RUNNING
-    print("gpsd receive loop started")
-    data_stream = gps3.DataStream()
-    gpsd_socket.watch()
-    for new_data in gpsd_socket:
-        if new_data:
-            if RUNNING == False:
-                return
-            data_stream.unpack(new_data)
-            print("GPSD data....")
+    def loop(self, gpsd_socket):
+        print("gpsd receive loop started")
+        data_stream = gps3.DataStream()
+        gpsd_socket.watch()
+        for new_data in gpsd_socket:
+            if new_data:
+                if self.running == False:
+                    return
+                data_stream.unpack(new_data)
+                print("GPSD data....")
 
-            lock.acquire()
-            position['lat']=data_stream.TPV['lat']
-            position['lon']=data_stream.TPV['lon']            
-            position['alt']=data_stream.TPV['alt']
-            position['speed']=data_stream.TPV['speed']
-            if "hdop" in data_stream.SKY:
-                position['hdop']=data_stream.SKY['hdop']
+                self.lock.acquire()
+                self.position['lat']=data_stream.TPV['lat']
+                self.position['lon']=data_stream.TPV['lon']            
+                self.position['alt']=data_stream.TPV['alt']
+                self.position['speed']=data_stream.TPV['speed']
+                if "hdop" in data_stream.SKY:
+                    self.position['hdop']=data_stream.SKY['hdop']
 
-            #check if position valid
-            if 'n/a' in position.values():
-                position['valid']=False
-            else:
-                position['valid']=True
-            lock.release()
-
-
-def getPosition():
-    global position, lock
-    lock.acquire()
-    p=position
-    lock.release()
-    return p
-
-
-def connect(host,port):
-    gpsd_socket = gps3.GPSDSocket()
-    gpsd_socket.connect(host, port)  #TODO: Find out when it fails
-
-#    loop(gpsd_socket)
-    t = threading.Thread(target=loop, args=(gpsd_socket,))
-    t.start()
-    
+                #check if self.position valid
+                if 'n/a' in self.position.values():
+                    self.position['valid']=False
+                else:
+                    self.position['valid']=True
+                self.lock.release()
 
 
-def shutdown():
-	global RUNNING
-	RUNNING=False
+    def getPosition(self):
+        self.lock.acquire()
+        p=self.position
+        self.lock.release()
+        return p
 
 
-def initProvider(config):
-    print("Init gpsd provider...")
-    if "Provider.gpsd" not in config:
-        print("Provider.gpsd section missing from configuration, exiting")
-        sys.exit(-1)
-    
-    provider_config=config['Provider.gpsd']
-    gpsd_host=provider_config.get('host','127.0.0.1')
-    gpsd_port=provider_config.get('port','2947')
+    def connect(self,host,port):
+        gpsd_socket = gps3.GPSDSocket()
+        gpsd_socket.connect(host, port)  #TODO: Find out when it fails
 
-    print("Trying to connect gpsd at "+str(gpsd_host)+" port "+str(gpsd_port))
-    connect(gpsd_host,gpsd_port)
+    #    loop(gpsd_socket)
+        t = threading.Thread(target=self.loop, args=(gpsd_socket,))
+        t.start()
+        
 
-    
+
+    def shutdown(self):
+        self.running=False
+
+
