fcc1a2ca09099b9a6c9036b570b3cf818fe6bb68
Chen Wenwen (CR/AME5) <Wenwen.Chen@de.bosch.com>
2020-09-17 16:24:08 +0200

    add kuksa provider
    Signed-off-by: Chen Wenwen (CR/AME5) <Wenwen.Chen@de.bosch.com>

--- invehicle-apps/kuksa-traccar-client/providers/kuksa_val.py
+++ invehicle-apps/kuksa-traccar-client/providers/kuksa_val.py
@@ -15,113 +15,73 @@
 # All other values will be reported as 0
 
 
+import os, sys, json
 import csv
 import time
 import threading, queue, ssl
 import asyncio, websockets, pathlib
+from providers.kuksa.clientComm import VSSClientComm
 
-position = { "valid":False, "lat":"0", "lon":"0", "alt":"0", "hdop":"0", "speed":"0" }
-lock=threading.Lock()
-RUNNING=False
-
-class VSSProvider(threading.Thread):
+class Provider():
 
     # Constructor
     def __init__(self, config):
-        super(VSSClientComm, self).__init__()
-        provider_config=config['Provider.kuksa_val']
-        self.serverIP = provider_config.get('ip', "127.0.0.1")
-        self.serverPort = provider_config.get('port', 8090)
-        self.insecure = provider_config.get('insecure', false)
-        self.interval=provider_config.getint('interval',1)
-        self.cacertificate = provider_config.get('cacertificate', "CA.pem")
-        self.certificate = provider_config.get('certificate', "Client.pem")
-        self.keyfile = provider_config.get('key', "Client.key")
-        self.token = provider_config.get('token', "token.json")
         print("Init kuksa_val provider...")
         if "Provider.kuksa_val" not in config:
             print("Provider.kuksa_val section missing from configuration, exiting")
             sys.exit(-1)
-        
-        if not self.insecure:
-            context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-            context.load_cert_chain(certfile=self.certificate, keyfile=self.keyfile)
-            context.load_verify_locations(cafile=self.certificate)
-            try:
-                with websockets.connect("wss://"+self.serverIP+":"+str(self.serverPort), ssl=context) as ws:
-                    self.webSocket = ws
-            except OSError as e:
-                print("Disconnected!!" + str(e))
-                exit(-1)
-        else:
-            try:
-                async with websockets.connect("ws://"+self.serverIP+":"+str(self.serverPort)) as ws:
-                    self.webSocket = ws
-            except OSError:
-                print("Disconnected!!" + str(e))
-                exit(-1)
+        provider_config=config['Provider.kuksa_val']
+        self.sendMsgQueue = queue.Queue()
+        self.recvMsgQueue = queue.Queue()
+        self.client = VSSClientComm(self.sendMsgQueue, self.recvMsgQueue, provider_config)
+        self.client.start()
+        self.token = provider_config.get('token', "token.json")
         self.authorize()
-        global RUNNING
-        self.RUNNING = True
-
-    def stopComm(self):
-        global RUNNING
-        RUNNING = False
-
+        
     def authorize(self):
         if os.path.isfile(self.token):
-            with open(tokenOrFile, "r") as f:
-                token = f.readline()
-        else:
-            token = tokenOrFile
+            with open(self.token, "r") as f:
+                self.token = f.readline()
 
         req = {}
         req["requestId"] = 1238
         req["action"]= "authorize"
-        req["tokens"] = token
+        req["tokens"] = self.token
         jsonDump = json.dumps(req)
-        self.webSocket.send(req)
-        resp = self.webSocket.recv()
+        self.sendMsgQueue.put(jsonDump)
+        resp = self.recvMsgQueue.get(timeout = 1)
         print(resp)
 
-    def getValue(self, path):
+    def shutdown(self):
+        self.client.stopComm()
+
+    def getPosition(self):
         req = {}
         req["requestId"] = 1234
         req["action"]= "get"
-        req["path"] = path
+        req["path"] = "Vehicle.Cabin.Infotainment.Navigation.CurrentLocation"
         jsonDump = json.dumps(req)
-        self.webSocket.send(jsonDump)
-        resp = self.webSocket.recv()
-        print(resp)
+        print(jsonDump)
+        self.sendMsgQueue.put(jsonDump)
+        resp = self.recvMsgQueue.get(timeout = 1)
+    
+        position = { "valid": False }
+        resp = json.loads(resp)
 
-    def mainLoop(self):
-        global RUNNING
-        while RUNNING:
+        try:
+            data = resp['value']
+            position = { "valid":True, 
+                "alt": data['Vehicle.Cabin.Infotainment.Navigation.CurrentLocation.Altitude'],
+                "lat": data['Vehicle.Cabin.Infotainment.Navigation.CurrentLocation.Latitude'],
+                "lon": data['Vehicle.Cabin.Infotainment.Navigation.CurrentLocation.Longitude'],
+                "hdop": data['Vehicle.Cabin.Infotainment.Navigation.CurrentLocation.Accuracy'],
+                "speed": data['Vehicle.Cabin.Infotainment.Navigation.CurrentLocation.Speed']}
+        except TypeError as e:
+            print("type error: " + str(e))
+            pass
+        except :
+            print("Unexpected error:", sys.exc_info()[0])
             
-            global position, lock
-            datetime.sleep(self.interval)
+        
+        return position
 
-
-    # Thread function: Start the asyncio loop
-    def run(self):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(self.mainLoop())
-
-
-def initProvider(config):
-    t = VSSProvider(config)
-    t.start()
-    return t
-
-def shutdown():
-    global RUNNING
-    RUNNING=False
-
-
-def getPosition():
-    global position, lock
-    lock.acquire()
-    p=position
-    lock.release()
-    return p
